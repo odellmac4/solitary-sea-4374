7a7d1489031cff568b1c2b3f88d9619dc12d876f
Odell McFarland IV <odell.mac4@gmail.com>
2024-04-05 09:52:57 -0700

    Add member data in a module for readability

---
+++ spec/data/test_data.rb
@@ -0,0 +1,23 @@
+module TestData
+  def self.member_data
+    data = {
+      "_id": "5cf5679a915ecad153ab68da",
+      "allies": [
+          "Ozai",
+          "Zuko "
+      ],
+      "enemies": [
+          "Iroh",
+          "Zuko",
+          "Kuei",
+          "Long Feng",
+          "Mai",
+          "Ty Lee",
+          "Ursa "
+      ],
+      "photoUrl": "https://vignette.wikia.nocookie.net/avatar/images/1/12/Azula.png/revision/latest?cb=20140905084941",
+      "name": "Azula",
+      "affiliation": " Azula's team (formerly) Dai Li (formerly) Fire Nation Fire Nation Royal Family Fire Warriors Royal Fire Academy for Girls (formerly)"
+  }
+  end
+end
